af687d1e28c25afaa265a7a0ff86c702ccab1525
RudroMajumder <majumder.rudrofficial@gmail.com>
2021-01-22 01:47:20 +0600

    assignment

--- assignment.js
+++ assignment.js
@@ -32,38 +32,54 @@ function hotelCost(days){
     }
     else if ( days <= 10 ){
          total = days * 100;
-        return total;
+         return total;
     }
     else if ( days <= 20 ){
-       var firstTenDaysTotal = 10 * 100;
-       var extraDays = days - 10;
-       var extraDaysTotal = extraDays * 80 ;
-       total = firstTenDaysTotal + extraDaysTotal;
-       return total;
+        var firstTenDaysTotal = 10 * 100;
+        var extraDays = days - 10;
+        var extraDaysTotal = extraDays * 80 ;
+        total = firstTenDaysTotal + extraDaysTotal;
+        return total;
    }
    else{
-    var firstTenDaysTotal = 10 * 100;
-    var secondTenDaysTotal = 10 * 80;
-    extraDays = days - 20;
-    extraDaysTotal = extraDays * 50;
-    total = firstTenDaysTotal + secondTenDaysTotal+ extraDaysTotal;
-    return total;
-   } 
+        var firstTenDaysTotal = 10 * 100;
+        var secondTenDaysTotal = 10 * 80;
+        extraDays = days - 20;
+        extraDaysTotal = extraDays * 50;
+        total = firstTenDaysTotal + secondTenDaysTotal+ extraDaysTotal;
+        return total;
+    } 
 }
 var cost = hotelCost(50);
 console.log(cost);
 
 //============================function for megaFriend========================================
-function megaFriend( friend ){
+// function megaFriend( friend ){
+//    var length = friend[0].length;
+//    var max = friends[0];
+//      for( var i = 0; i < friend.length; i++){
+//         var size = friend[i].length;
+//         if( size > length){
+//              max = friend[i];
+//              length = size;
+//          }
+//      }    return max; 
+//     }
+//  var friends = ['Rudro','Topu','amar nam hero alomer bap ','Jihan','amar nam hero alomer dada ','ABCDEFGHJIJJ','Mohiuddin','RAihan'];
+//  var result = megaFriend( friends);
+//  console.log(result);
+function megaFriend(friend){
     var length = friend[0].length;
-    for( var i = 0; i < friend.length; i++){
+    var max = friends[0];
+    for( var i =0; i < friend.length; i++){
         var size = friend[i].length;
-        if( size > length){
-            var max = friend[i];
-            return max;
+        if( size > length ){
+            max = friend[i];
+            length = size;
         }
     }
+    return max;
 }
-var friends = ['Rudro','amar nam hero alom','Topu','Jihan','ABCDEFGHJIJJ','Mohiuddin','RAihan'];
-var result = megaFriend( friends);
+var friends = ['RUDRO', 'TOPU', 'HERO ALOM', 'JIHAN', 'IZHARUL','RAIHAN'];
+var result = megaFriend(friends);
 console.log(result);
